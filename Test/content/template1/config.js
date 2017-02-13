25bed93078b620445d2224dc016e782ac9eec698
Sharif Mehedi Tanmay <sharif_mehdia@yahoo.com>
2017-02-13 12:38:21 -0600

    testing

---
+++ Test/content/template1/config.js
@@ -0,0 +1,2 @@
+{ "tid" : "t01","tdescHeader" : "Template 1:  Single SCO","tdesc" : "This is the most basic SCO structure.  A root aggregation contains a single SCO.  The SCO may be any size and have any amount of intra-SCO branching or an assessment.  This SCO contains one asset."}
+                   
---
+++ Test/content/template9/config.js
@@ -0,0 +1,3 @@
+{ "tid" : "t09","tdescHeader" : "Template 9:  Basic Three-Way Branching","tdesc":"Template 9 shows how you can use simple sequencing rules to accomplish basic adaptive inter-SCO sequencing that is similar to the branching you might have used in traditional CBT lessons.  Based upon the learner’s choice or decision, represented as a normalized score between –1 and +1, the learner would be directed to another SCO.  " }
+
+                   
