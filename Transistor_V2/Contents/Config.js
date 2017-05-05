5ea3c7947d1632bce6d849e3de609184014d9f03
C. Mcglown <cjmcglwn@memphis.edu>
2017-05-04 23:57:58 -0500

    Update

---
+++ Transistor_V2/Contents/Config.js
@@ -0,0 +1,44 @@
+
+var configResourceURI = 'http://activity.com/';
+var configKnowledgeComponentURI = 'http://example.com/KC/';
+var configUsers = 
+[
+'Joe@gmail.com',
+'Jane@school.edu',
+'Jordan@gmail.com',
+'Thomas@hotmail.com',
+'Kate@hotmail.com',
+'Sally@gmail.com'
+];
+
+var configResources = 
+[
+'autotutor',
+'autotutorDRQ',
+'autotutorKC',
+'dragoon',
+'bbn'
+];
+
+//We use only xAPI options only as of now.
+var configVerbs = 
+[
+'passed',
+'failed',
+'mastered',
+'answered',
+'answered',
+'completed',
+'progressed',
+'scored'
+];
+
+var configKnowledgeComponents = 
+[
+'PN Junctions',
+'Electrons',
+'Parallel',
+'Series',
+'Thevonen',
+'Voltage Potential'
+];
