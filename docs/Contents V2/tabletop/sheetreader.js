feac6d48a78711cca6616e4fd27fd49b41c0478a
C. Mcglown <cjmcglwn@memphis.edu>
2017-06-12 13:25:54 -0500

    Recomendations are pulling from real spreadsheet.
    Bugs cleaned up

--- docs/Contents V2/tabletop/sheetreader.js
+++ docs/Contents V2/tabletop/sheetreader.js
@@ -1,5 +1,8 @@
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1gwmaZdyLonPG4hgiri-e20GEa0MgSCLwZOhix3CEsvE/pubhtml';
+//working example://var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1gwmaZdyLonPG4hgiri-e20GEa0MgSCLwZOhix3CEsvE/pubhtml';
+//replicated ET problem sheet
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1j1j1s8WqpXke5-dsNRoqmBsZZ0QtnjWOO1rYqw-5ZmE/pubhtml';
 var sheetReaderData = '';
+
 
       function init() {
         Tabletop.init( { key: public_spreadsheet_url,
